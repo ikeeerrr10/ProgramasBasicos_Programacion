da02189f4445d2d3f81c813d50cd104195bf5747
Iker Suárez López <ikersl20@educastur.es>
2025-12-02 20:03:01 +0100

    Add files via upload

---
+++ E8_IkerSuarez.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        boolean estaVacia=false;
+        int edad=20;
+        float facturaLuz= 180.3F;
+        char estadoCivil='C';
+        String nombreAsignatura= "Programación";
+        double areaFigura=278.5;
+        boolean estáAprobado=true;
+
+        System.out.println("El area de la figura es:"+areaFigura+"y mi estado civil es:"+estadoCivil);
+    }
+}
---
+++ E9_IkerSuarez.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int radio = 6;
+        int diametro = radio*2;
+        final double PI = 3.14;
+        double perimetro = diametro * PI;
+        double area = PI * (radio*radio);
+
+        System.out.println("El radio del circulo es:"+radio);
+        System.out.println("El perímetro del círculo es:" + perimetro);
+        System.out.println("El área del círculo es:" + area);
+
+    }
+}
